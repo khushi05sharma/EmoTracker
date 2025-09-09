6e27e6350a7d347b3c8c6f98f8374f64d75272a5
khushi05sharma <5103.sharmakhushi@gmail.com>
2025-09-09 19:49:25 +0530

    changes in header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,17 +1,14 @@
-import { Link } from 'react-router-dom';
-import './Header.css';
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 export default function Header() {
   return (
     <header className="header">
       {/* Use Link for navigation */}
-      <Link to="/past-notes" className="pastBtn">Past Notes</Link>
+      <Link to="/past-notes" className="pastBtn">
+        Past Notes
+      </Link>
       <h1 className="moodHead">EmoTracker</h1>
     </header>
   );
 }
-
-
-
-
-
